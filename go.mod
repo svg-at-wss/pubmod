4b6a4a6be55dc30fe28f586712818e03da9cbcd7
sharkness <samantha.gray@whitesourcesoftware.com>
2022-05-19 12:12:14 -0500

    add minify func

--- go.mod
+++ go.mod
@@ -2,9 +2,6 @@ module github.com/svg-at-wss/pubmod
 
 go 1.18
 
-require (
-	github.com/bxcodec/faker/v3 v3.8.0 // indirect
-	github.com/svg-at-wss/secretmod v0.0.0-20220519163042-f4a6b3bedada // indirect
-	github.com/tdewolff/minify/v2 v2.11.2 // indirect
-	github.com/tdewolff/parse/v2 v2.5.29 // indirect
-)
+require github.com/tdewolff/minify/v2 v2.11.2 // direct
+
+require github.com/tdewolff/parse/v2 v2.5.29 // indirect
--- pubmod.go
+++ pubmod.go
@@ -1,7 +1,14 @@
 package pubmod
 
-import "minify"
-import "secretmod"
+import (
+	"github.com/tdewolff/minify/v2"
+	"github.com/tdewolff/minify/v2/css"
+	"github.com/tdewolff/minify/v2/html"
+	"github.com/tdewolff/minify/v2/js"
+	"github.com/tdewolff/minify/v2/json"
+	"github.com/tdewolff/minify/v2/svg"
+	"github.com/tdewolff/minify/v2/xml"
+)
 
 func Hello() string {
   return "Hello, You!"
@@ -9,4 +16,18 @@ func Hello() string {
 
 func Goodbye() string {
   return "Goodbye for now!"
+}
+
+func myminify() {
+	m := minify.New()
+	m.AddFunc("text/css", css.Minify)
+	m.AddFunc("text/html", html.Minify)
+	m.AddFunc("image/svg+xml", svg.Minify)
+	m.AddFuncRegexp(regexp.MustCompile("^(application|text)/(x-)?(java|ecma)script$"), js.Minify)
+	m.AddFuncRegexp(regexp.MustCompile("[/+]json$"), json.Minify)
+	m.AddFuncRegexp(regexp.MustCompile("[/+]xml$"), xml.Minify)
+
+	if err := m.Minify("text/html", os.Stdout, os.Stdin); err != nil {
+		panic(err)
+	}
 }
